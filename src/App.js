7f6f7cf47a8c1ddfedf9b401e14a2a9752753ba5
kirkby <kirkby@neighbor.com>
2021-02-17 21:07:34 -0700

    new cat pic

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <main>
-      <h1>Cat Pics!!!!!</h1>
-      <div><img alt="cat" style={{ maxWidth: '700px' }} src="https://www.rd.com/wp-content/uploads/2019/04/shutterstock_1013848126.jp" /></div>
+      <h1>Cat Pics!!!!!</h1>0
+      <div><img alt="cat" style={{ maxWidth: '700px' }} src="https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/119057301_3273521859395063_7519037250371767454_n.jpg?_nc_cat=109&ccb=3&_nc_sid=8bfeb9&_nc_ohc=rXCDrp9dgdAAX9ha3q0&_nc_ht=scontent-den4-1.xx&oh=30496897f4fec32ed3f0e04a252f180b&oe=60543DCB" /></div>
     </main>
   );
 }

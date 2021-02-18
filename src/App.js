3a68cddd1137ca5f2e26141b1a7337cbbb744081
kirkby <kirkby@neighbor.com>
2021-02-17 20:49:56 -0700

    removed redundant alt text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <main>
       <h1>Cat Pics!!!!!</h1>
-      <div><img alt="cat picture" style={{ maxWidth: '700px' }} src="https://www.rd.com/wp-content/uploads/2019/04/shutterstock_1013848126.jpg" /></div>
+      <div><img alt="cat" style={{ maxWidth: '700px' }} src="https://www.rd.com/wp-content/uploads/2019/04/shutterstock_1013848126.jpg" /></div>
     </main>
   );
 }

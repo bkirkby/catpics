a30939e54aed1a0a714b83af8dc4372b3725ec05
kirkby <kirkby@neighbor.com>
2021-04-14 20:57:20 -0600

    using a dog image for a cat

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <main>
       <h1>Memetic Cat Pics!!!!!</h1>
-      <div><img alt="cat" style={{ maxWidth: '700px' }} src="https://www.rd.com/wp-content/uploads/2019/04/shutterstock_1013848126.jpg" /></div>
+      <div><img alt="cat" style={{ maxWidth: '700px' }} src="https://secure.img1-fg.wfcdn.com/im/85627489/resize-h800%5Ecompr-r85/7006/70060506/Dogs+and+Puppies+%2527Mona+Lisa+Pet%2527+Graphic+Art+Print+on+Wrapped+Canvas.jpg" /></div>
     </main>
   );
 }
